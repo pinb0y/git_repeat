34b3ce47c26e4f01b4ecee51bca7d9962da04e44
Pinchuk Sergey <Pinboyer@gmail.com>
2024-07-13 18:31:13 +0300

    add new foo

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 def capitalizer(str):
+    """docstring"""
     result = str.upper()
+    return result
+
+
+def first_letter_up(str):
+    """docstring"""
+    result = str.capitalize()
     return result
